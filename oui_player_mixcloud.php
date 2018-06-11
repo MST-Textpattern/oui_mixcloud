2f42934a8a66df9f59b3df9fc1ce962bed826e39
Nicolas Morand <contact@nicolasmorand.com>
2018-06-11 16:58:44 +0200

    Fixes related to oui_player v2 rework.

--- oui_player_mixcloud.php
+++ oui_player_mixcloud.php
@@ -74,10 +74,6 @@ namespace Oui\Player {
             );
         }
 
-        global $event;
-
-        if (txpinterface === 'admin' && ($event === 'prefs' || $event === 'plugin_prefs.oui_player_mixcloud')) {
-            Mixcloud::getInstance();
-        }
+        register_callback('Oui\Player\Mixcloud::getProvider', 'oui_player', 'plug_providers');
     }
 }
