b1ff4a3af6064674a843f3cc039560cebee5c5c6
Nicolas Morand <contact@nicolasmorand.com>
2018-08-20 14:41:26 +0200

    Updates the Provider class namespace

--- oui_mixcloud.php
+++ oui_mixcloud.php
@@ -31,9 +31,9 @@
 
 namespace Oui;
 
-if (class_exists('Oui\Provider')) {
+if (class_exists('Oui\Player\Provider')) {
 
-    class Mixcloud extends Provider
+    class Mixcloud extends Player\Provider
     {
         protected static $srcBase = '//www.mixcloud.com/';
         protected static $srcGlue = array('widget/iframe/?feed=', '?', '&amp;');
